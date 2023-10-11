5fc3d3584b17004c29d71cdf9533673528bfbeed
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-10-11 09:38:56 -0400

    finished product

--- main.ts
+++ main.ts
@@ -14,34 +14,50 @@ let score: number
 
 input.onGesture(Gesture.Shake, function() {
   randomNumber = randint(1,3)
+
+ // rock
+  if (randomNumber = 1) {
+  basic.showLeds(`
+  . . . . .
+  . # # # .
+  . # # # .
+  . # # # .
+  . . . . .
+  `)
+  basic.pause(1000)
+  }
+
+ // paper
+  if (randomNumber = 2) {
+  basic.showLeds(`
+  # # # # #
+  # . . . #
+  # . . . #
+  # . . . #
+  # # # # #
+  `)
+  basic.pause(1000)
+  }
+
+ // scissors
+  if (randomNumber = 3) {
+  basic.showLeds(`
+  # # . . .
+  # # . # #
+  . # # # .
+  # # . # #
+  # # . . .
+  `)
+  basic.pause(1000)
+  }
+
+  input.onButtonPressed(Button.A, function () {
+  score = score + 1
+  basic.showNumber(score)
+  })
+
+  input.onButtonPressed(Button.B, function () {
+  score = 0
+  basic.showNumber(score)
 })
-
-if (randomNumber = 1) {
-  basic.showLeds(`
-  . . . . .
-  . # # # .
-  . # # # .
-  . # # # .
-  . . . . .
-  `)
-}
-
-if (randomNumber = 2) {
-  basic.showLeds(`
-  # # # # #
-  # . . . #
-  # . . . #
-  # . . . #
-  # # # # #
-  `)
-}
-
-if (randomNumber = 3) {
-    basic.showLeds(`
-  # # . . .
-  # # . # #
-  . # # # .
-  # # . # #
-  # # . . .
-  `)
-}
+})
